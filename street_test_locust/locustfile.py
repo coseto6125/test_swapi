58d0f34415b39c24a4088647146929a9ca7b920e
coseto6125 <enorenor@gmail.com>
2021-12-21 21:43:11 +0800

    locustfile__Vehicles_size/r.status_code

--- street_test_locust/locustfile.py
+++ street_test_locust/locustfile.py
@@ -1,11 +1,20 @@
-﻿from locust import HttpUser, task, between
+﻿from locust import HttpUser, task
+from locust.user.wait_time import constant_pacing
+from locust.contrib.fasthttp import FastHttpUser
 
+#class Api_User(FastHttpUser):
 class Api_User(HttpUser):
-    wait_time = between(1, 1)
-
+    wait_time = constant_pacing(1)
     @task
-    def index(self):
-        self.client.get(f'/people/1/')
-
+    def test_locust(self):
+        with self.client.get("https://swapi.dev/api/people/1/",catch_response=True) as r:
+            Vehicles_size = len(r.json()['vehicles'])
+            assert Vehicles_size >= 1                       #------Vehicles_size >= 1
+            assert r.status_code == 200                     #------http status check == 200
+            if r.status_code ==200:                         #------http status check != 200 will be [Failed!]
+                r.success()
+            else:
+                r.failure("GetActConfig[Failed!]")
+                
 #locust -f locustfile.py -H https://swapi.dev/api --headless -u 10 -r 10 -t 600s
-#locust -f ./street_test_locust/locustfile.py -H https://swapi.dev/api --headless -u 10 -r 1 -t 10s
+#locust -f ./street_test_locust/locustfile.py -H https://swapi.dev/api --headless -u 10 -r 10 -t 10s
